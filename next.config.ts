96a5c05de237f6cfbd403819d231eaabc306b63e
The NS Guardians <nihalrajpoot0987@gmail.com>
2025-10-05 10:36:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,8 +31,9 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    allowedDevOrigins: ['*.cloudworkstations.dev'],
+    //
   },
+  allowedDevOrigins: ['*.cloudworkstations.dev'],
 };
 
 export default nextConfig;
